f6b52d212de6c934c78d77d61119c7eb10f9ac3e
Kamil <ckkomancz18@yahoo.com>
2021-05-03 12:03:38 +0200

    add Dish and Menu models

--- eMenu/models.py
+++ eMenu/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Dish(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=200)
+    price = models.IntegerField()
+    minutes_to_prepare = models.IntegerField()
+    creation_date = models.DateTimeField(default=timezone.now, editable=False)
+    edition_date = models.DateTimeField(blank=True, null=True)
+    is_vegetarian = models.BooleanField(default=False)
+    picture = models.ImageField(upload_to='dishes', default='default.jpg')   #TODO add default here
+        
+    ## return it in a way you want it to be printed out   
+    def __str__(self):
+        return f'Dish: {self.name}'
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    description = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(default=timezone.now, editable=False)
+    edition_date = models.DateTimeField(blank=True, null=True)
+    dishes = models.ManyToManyField(Dish)
+
+    ## return it in a way you want it to be printed out   
+    def __str__(self):
+        return f'Menu: {self.name}'
