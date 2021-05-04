d15957ea2027c947205c356608e1aff28b938fbd
Kamil <ckkomancz18@yahoo.com>
2021-05-04 20:45:19 +0200

    add POST, PUT, DELETE methods for Dish & Menu objects

--- eMenu/urls.py
+++ eMenu/urls.py
@@ -1,20 +1,25 @@
-  
-from django.urls import path, re_path
-from . import views
-
+from django.urls import path, re_path, include
+from rest_framework.routers import DefaultRouter
+from . import api_views
+from .models import *
 
 app_name = 'short_texts'
 
-urlpatterns = [
-    ## API overview
-	path('', views.api_overview, name='api-overview'),
 
+urlpatterns = [
     ## eMenu app dish/list_dishes?sort=name
     # Dish model CRUD
-    re_path(r'^dish/list_dishes/$', views.get_dishes, name='list_dishes'),      # can be sorted be any field (e.g. name), e.g. http://localhost:8000/api/eMenu/menu/list_menus/?sort=dishes; http://localhost:8000/api/eMenu/dish/list_dishes/?sort=edition_date
-    re_path(r'dish/dish_details(?P<pk>\w+|)/$', views.get_dish_details, name='get_dish_details'),
+    re_path(r'^dish/list_dishes/$', api_views.list_dishes, name='list_dishes'),      # can be sorted be any field (e.g. name), e.g. http://localhost:8000/api/eMenu/menu/list_menus/?sort=dishes; http://localhost:8000/api/eMenu/dish/list_dishes/?sort=edition_date
+    re_path(r'^dish/dish_details/(?P<pk>\w+)/$', api_views.retrieve_dish_details, name='retrieve_dish_details'),
+    re_path(r'^dish/(?P<pk>\w+)/$', api_views.DishView.as_view(), name='update-delete-dish'),
+    re_path(r'^dish/$', api_views.DishCreateView.as_view(), name='create-dish'),
 
     # Menu model CRUD
-    re_path(r'^menu/list_menus/$', views.get_menus, name='list_menus'),         # can be sorted be any field (e.g. number of dishes)
-	re_path(r'menu/menu_details(?P<pk>\w+|)/$', views.get_menu_details, name='get_menu_details'),
+    re_path(r'^menu/list_menus/$', api_views.list_menus, name='list_menus'),         # can be sorted be any field (e.g. number of dishes)
+	re_path(r'^menu/menu_details(?P<pk>\w+)/$', api_views.retrieve_menu_details, name='retrieve_menu_details'),
+    re_path(r'^menu/(?P<pk>\w+)/$', api_views.MenuView.as_view(), name='update-delete-menu'),
+    re_path(r'^menu/$', api_views.MenuCreateView.as_view(), name='create-menu'),
+
 ]
+
+#  non-capturing group in the regex:        (?:/(?P<title>[a-zA-Z]+)/)?
